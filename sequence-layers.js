a74415c93aabb6e70786568e56ce8fd7bd8025bc
Cortland Mahoney <cortland.mahoney@gmail.com>
2023-08-27 18:22:42 -0400

    Update sequence-layers.js

--- sequence-layers.js
+++ sequence-layers.js
@@ -1,14 +1,14 @@
 s0.initVideo("https://raw.githubusercontent.com/ckmahoney/algorave-video/main/algorave-futures-lab.mp4")
 
-osc().color(1,0,0).out(o1)
-voronoi().color(0,1,0).out(o2)
-noise().color(0,0,1).out(o3)
+noise(0.01).color(0,0,1).add(osc(300).color($=>Math.abs(Math.sin(time/5)),$=>Math.abs(Math.sin(time/13)),0).diff(src(s2)).contrast().brightness(0.01)).out(o1)
+voronoi(70).posterize(32).color(0,$=>Math.abs(Math.sin(time/10))/2,$=>Math.abs(Math.sin(time/3))).out(o2)
+noise(0.01).color($=>Math.abs(Math.sin(time/10))/2,0,$=>Math.abs(Math.sin(time/5))/3).out(o3)
 
-let stage = (r) => (_) => Math.abs(Math.sin(Math.PI*r*time/100))
 
+let stage = (phase, slower=100) => (_) => Math.abs(Math.sin(Math.PI*phase*time/slower))
 
 solid()
-  .blend(src(o1),stage(0))
+  .blend(src(o1),stage(Math.PI*1/3))
   .blend(src(o2),stage(Math.PI*2/3))
-  .blend(src(o3),stage(Math.PI*4/3))
-  .diff(src(s0)).out()
+  .blend(src(o3),stage(Math.PI*3/3))
+  .diff(src(s0).rotate($=>Math.sin(time/1000) * 2/3)).out()
